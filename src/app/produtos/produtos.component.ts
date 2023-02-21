254fc400038897c3b8414e246a39d64116892ba9
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-02-21 00:33:34 -0300

    Criando um componente separado para a barra de pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduto, produtos } from '../produtos';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from '../produtos';
 import { ProdutosService } from '../produtos.service';
 
 @Component({
@@ -11,11 +12,23 @@ export class ProdutosComponent implements OnInit {
   produtos: IProduto[] | undefined;
 
   constructor(
-    private produtoService: ProdutosService
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.produtos = this.produtoService.getAll();
+    const produtos = this.produtoService.getAll();
+
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+      if (descricao) {
+        this.produtos = produtos.filter(produto => produto.descricao.toLocaleLowerCase().includes(descricao));
+        return;
+      }
+
+      this.produtos = produtos;
+    });
   }
 
 }
